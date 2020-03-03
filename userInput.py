6c711c9dbf40396cc6f6ca42775f613f3ad7b324
tjwill95 <33507928+tjwill95@users.noreply.github.com>
2020-03-02 16:28:43 -0800

    Update userInput.py

--- userInput.py
+++ userInput.py
@@ -4,8 +4,8 @@ SUPPORT = False      #Generates support structure
 SEPARATE_SUPPORTS = True #Spits out two files, one for the support and one for the object
 PERFORATE = False    #Perforates the support structure to allow fluids into the support cells
 IMG_STACK = False   #Outputs an image stack of the model
-AESTHETIC = True   #Removes all internal detail, works best with INVERSE
-INVERSE = True     #Also includes the inverse of the model as a separate file
+AESTHETIC = False   #Removes all internal detail, works best with INVERSE
+INVERSE = False     #Also includes the inverse of the model as a separate file
 NET = False          #Only draws voronoi patterns at the surface of the objec
 SMOOTH = True       #Smooths the output meshes, removes the voxelized texture
 NET_THICKNESS = 4   #Sets the thickness of the net in voxels
@@ -13,9 +13,9 @@ BUFFER = 4          #Sets the empty voxels around the object
 TPB = 8             #Threads per block, leave at 8 unless futzing.
 
 RESOLUTION = 300    #Sets the resolution of the Y and Z axes
-MODEL_THRESH = 0.05  #Influences the number of cells in the model, larger values lead to more cells
-MODEL_SHELL = 0#3     #Sets the thickness of the model skin (in voxels), set to 0 for aesthetic models
-MODEL_CELL = 7#0.9     #Sets the thickness of the cell walls within the model (in voxels)
+MODEL_THRESH = 0.1  #Influences the number of cells in the model, larger values lead to more cells
+MODEL_SHELL = 3#3     #Sets the thickness of the model skin (in voxels), set to 0 for aesthetic models
+MODEL_CELL = 0.9#0.9     #Sets the thickness of the cell walls within the model (in voxels)
 
 SUPPORT_THRESH = 0.2#Influences the number of cells in the supports, larger values lead to more cells
 SUPPORT_CELL = 0.7  #Sets the thickness of the cell walls within the supports (in voxels)
@@ -26,17 +26,15 @@ SUPPORT_CELL = 0.7  #Sets the thickness of the cell walls within the supports (i
 
 #FILE_NAME = "E.stl"
 #FILE_NAME = "3DBenchy_up.stl"
-FILE_NAME = "synx.stl"
 #FILE_NAME = "3DBenchy.stl"
 #FILE_NAME = "bust_low.stl"
 #FILE_NAME = "wavySurface.stl"
-#FILE_NAME = "luckyCat.stl"
 
 #If you would prefer a simple geometric object, uncomment the one you want and
 #make sure that all FILE_NAME options are commented out.
 
 #PRIMITIVE_TYPE = "Heart"
-#PRIMITIVE_TYPE = "Egg"
+PRIMITIVE_TYPE = "Egg"
 #PRIMITIVE_TYPE = "Cube"
 #PRIMITIVE_TYPE = "Sphere"
 #PRIMITIVE_TYPE = "Cylinder"
